cbed90a273ec0c4aeb7b1b30b235c992181fe5eb
Lars Haugan <lars@larshaugan.net>
2014-09-08 12:42:44 +0000

    Implemented restart, compile ++ in haproxy

--- haproxy/haproxy.py
+++ haproxy/haproxy.py
@@ -2,37 +2,63 @@
 # -*- coding: utf-8 -*-
 
 from subprocess import Popen
-from conn import HAconn
+#from conn import HAconn
+from os import path
+from jinja2 import Environment, PackageLoader, FileSystemLoader
 
 socket = "/var/run/haproxy/admin.sock"
+class HAproxy:
+    def __init__(self):
+        self.subnet = 'MS016A_net'
+        pass
 
-def restart():
-    """ """
-    # Need to collect the current sessions before restart!
-    # service haproxy reload # < will reload the config with minimal service impact
-    # Needs to be run with root privileges
-    pr = subprocess.Popen("service haproxy reload".split(), stdout=subprocess.PIPE)
-    output, err = pr.communicate()
+    def restart(self):
+        """ """
+        # Need to collect the current sessions before restart!
+        # service haproxy reload # < will reload the config with minimal service impact
+        # Needs to be run with root privileges
+        pr = subprocess.Popen("service haproxy reload".split(), stdout=subprocess.PIPE)
+        output, err = pr.communicate()
 
-    if not err:
-        return True
-    else:
-        return False
+        if not err:
+            return True
+        else:
+            return False
 
-def compile():
-    pass
+    def compile(self, serverlist):
+        #http://jinja.pocoo.org/docs/dev/api/
+        p = path.dirname(path.abspath(__file__))
+        env = Environment(loader=FileSystemLoader(path.split(p)[0] + '/etc/'))
+        template = env.get_template('haproxy.cfg')
 
-def set_online(host):
-    conn = HAconn(socket)
-    send_cmd()
-    pass
+        nodes = []
+        counter = 1
+        for server in serverlist:
+            s = {}
+            s['name'] = server.name
+            s['ip'] = server.addresses[self.subnet][0]['addr']
+            s['id'] = counter
+            counter += 1
+            nodes.append(s)
 
-def set_offline(host):
-    pass
+        #nodes=[ {'name': 'node01', 'ip': '192.168.128.48','id':1},
+                #{'name': 'node02', 'ip': '192.168.128.50','id':2},
+                #{'name': 'node03', 'ip': '192.168.128.51','id':3}]
+        print template.render(nodes=nodes)
+
+
+    def set_online(self,host):
+        conn = HAconn(socket)
+        send_cmd()
+        pass
+
+    def set_offline(self,host):
+        pass
 
 
 def main():
-    print restart()
+    ha = HAproxy()
+    ha.compile()
 
 if __name__ == '__main__':
     main()
