8035225fbcfcebe3e7425f875e9acbec40739a77
Lars Haugan <lars@larshaugan.net>
2014-09-08 12:43:18 +0000

    Added posibility to get backends (named node)

--- nova.py
+++ nova.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from novaclient import client
-from os import environ,path
+from os import environ, path
 import ConfigParser
-
 
 class openstack:
 
     def __init__(self):
-        if path.isfile('etc/openstack.cfg'):
+        p = path.dirname(path.abspath(__file__))
+        p = path.join(p, 'etc/openstack.cfg')
+        if path.isfile(p):
             config = ConfigParser.ConfigParser()
-            config.read('etc/openstack.cfg')
+            config.read(p)
         else:
             logging.error("Missing configuration file 'etc/openstack.cfg'")
             exit(1)
@@ -25,46 +26,57 @@ class openstack:
 
         self.nova = client.Client(**self.cred)
 
-    #def get_nova_creds():
-        #c = {}
-        #c['version'] = '2'
-        #c['username'] = environ['OS_USERNAME']
-        #c['api_key'] = environ['OS_PASSWORD']
-        #c['auth_url'] = environ['OS_AUTH_URL']
-        #c['project_id'] = environ['OS_TENANT_NAME']
-        #return c
-
-    def get_backends():
+    def backends(self):
         """ Get the virtual machines that are backends """
+        nodes = self.nova.servers.list()
+        backends = []
+
+        for node in nodes:
+            if "node" in node.name:
+                backends.append(node)
+
+        #print backends
+        #print dir(backends[0])
+        #print backends[0].addresses
+        #print backends[0].to_dict()
+        #print backends[0].addresses
+        return backends
+        #print nodes
+        #print dir(nodes[0])
+        #print nodes[0].to_dict()
+        #print nodes[0].addresses
+        #for node in nodes:
+            #print node
+
+    def get_status(self):
+        """ Get the machine status """
         pass
 
-    def get_status():
+    def get_info(self):
+        """ Get information of a instnace.
+            Returns: Dict with at least IP if present
+        """
         pass
 
-    #creds = get_nova_creds()
-    #nova = client.Client(**creds)
+    def create_web(self):
+        """ Creates a instance in Openstack """
+        pass
+
+    def terminate(self,machine):
+        """ Terminates a instance """
+        pass
+
+def main():
+    stack = openstack()
+    stack.backends()
+    #print stack.cred
+
+    #nova = client.Client(**stack.cred)
     #print nova.flavors.list()
     #print nova.servers.list()[0].status
     #print nova.servers.list()[0].addresses
     #print nova.limits.get().to_dict()
     #print nova.quotas.get('59a46c9fcf174ec3890211cc86e0836b', user_id='s171201').instances
-
-    def create_web():
-        pass
-
-    def terminate():
-        pass
-
-def main():
-    stack = openstack()
-    print stack.cred
-
-    #creds = get_nova_creds()
-    nova = client.Client(**stack.cred)
-    print nova.flavors.list()
-    print nova.servers.list()[0].status
-    print nova.servers.list()[0].addresses
-    print nova.limits.get().to_dict()
 
 if __name__ == '__main__':
     main()
