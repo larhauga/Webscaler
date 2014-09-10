9d5885c81838894bf6d36e700ba73eb90bbd2afd
Lars Haugan <lars@larshaugan.net>
2014-09-10 21:19:06 +0000

    Work in progress: scale_up. main now rebuilds configuration dependent on running instances

--- webscaler.py
+++ webscaler.py
@@ -11,12 +11,26 @@ min_backends = 2
 # figure out how to get the difference and keep count
 
 def scale_up():
+    stack = openstack()
+    sleeping = stack.sleeping_machine()
+    if sleeping:
+        sleeping.start()
+    else:
+        new_instance = stack.create_backend()
+
+
+    ha = haproxy.HAproxy()
+
     # nova create server
     # get information
     # haproxy add server
     pass
 
 def scale_down():
+    ha = haproxy.HAproxy()
+    stack = openstack()
+    #backends = stack.backends()
+
     # haproxy set offline
     # wait untill no more connections
     # nova remove server
@@ -33,7 +47,8 @@ def main():
     stack = openstack()
     ha = haproxy.HAproxy()
 
-    ha.compile(stack.backends())
+    ha.compile(stack.active_backends())
+    ha.restart()
 
 if __name__ == '__main__':
     main()
