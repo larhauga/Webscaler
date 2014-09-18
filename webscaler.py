f9a28bb560be79e38cc10b34fb9435915ae5e0b3
Lars Haugan <lars@larshaugan.net>
2014-09-18 17:59:09 +0000

    Fixed scale_up, scaledown, compile, initiate, addmetrics and changes to main method

--- webscaler.py
+++ webscaler.py
@@ -15,6 +15,7 @@ server_threshold = 10
 current_backends = 0
 pending_backends = []
 quota_limit = 13
+hareloaded = False
 
 metrics = []
 actions = {}
@@ -43,7 +44,7 @@ def scale_up(Number=1):
     stack = openstack()
     sleeping = stack.sleeping_machine()
 
-    if len(stack.backends()) == (quota_limit-1):
+    if len(stack.backends()) == (quota_limit):
         return False
 
     scaled = 0
@@ -75,7 +76,12 @@ def scale_down(Number=1):
     print "active servs: %s, min bakends: %s" % (str(len(active)), str(min_backends))
 
     if len(active) <= min_backends:
+        if passive > 1:
+            for node in passive:
+                node.delete()
+            return True
         return False
+
     print passive
     print active
 
@@ -85,7 +91,7 @@ def scale_down(Number=1):
         for node in passive:
             print "Removing passive node %s" % node.name
             node.delete()
-            #removed += 1
+            removed += 1
 
     if not passive and len(active) == (min_backends+1):
         active[-1].stop()
@@ -94,12 +100,12 @@ def scale_down(Number=1):
     if active:
         print "Starting to remove active nodes, wanting to remove %s nodes" % Number
         left = len(active) - (Number-removed)
-        toremove = Number-removed
+        toremove = Number - removed - (min_backends)
 
         print "Number that will be left: %s" % str(left)
         if left > min_backends:
             print "Ok, so we are removing: %s hosts" % str(toremove)
-            for i in range(1,toremove+1):
+            for i in range(1,toremove):
                 print "Iteration %s" % str(i)
                 if 'ACTIVE' in active[-i].status:
                     print "Stopping node %s" % active[-i].name
@@ -107,6 +113,16 @@ def scale_down(Number=1):
                 elif 'SHUTOFF' in active[-i].status:
                     print "Deleting node &s" % active[-i].name
                     active[-i].delete()
+        elif left <= min_backends:
+            stack = openstack()
+            b = stack.backends()
+            remove = len(b) - min_backends
+            for i in range(1, remove+1):
+                if 'ACTIVE' in active[-i].status:
+                    print "Stopping node %s" % active[-i].name
+                    active[-i].stop()
+                else:
+                    print "Not removing %s since it is %s" % (active[-i].name, active[-i].status)
 
     return True
 
@@ -131,19 +147,22 @@ def update_conf():
         ha = haproxy.HAproxy()
         ha.compile(backends)
         ha.restart()
+        return True
+    return False
 
 def initiate():
     # Boot first machines if not active:
     stack = openstack()
     backends = stack.backends()
-    if len(backends) < min_backends:
-        scale_up(min_backends)
+    #if len(backends) < min_backends:
+        #scale_up(min_backends)
     # Gathering first data
     data = {}
     data['acu'] = hastats.get_backend_cum_requests()['stot']
     data['diff'] = 0
     data['date'] = datetime.datetime.now()
-    data['active'] = len(hastats.get_backends_up())
+    data['active'] = len(stack.active_backends())#len(hastats.get_backends_up())
+    data['haactive'] = len(hastats.get_backends_up())
     data['needed'] = None
     metrics.append(data)
     print metrics
@@ -154,8 +173,10 @@ def initiate():
     data['diff'] = (float(data['acu']) - float(last['acu'])) / float(sleeptime)
     data['date'] = datetime.datetime.now()
     data['needed'] = needed_servers(acu=data['acu'], diff=data['diff'])
-    data['active'] = len(hastats.get_backends_up())
+    data['active'] = len(stack.active_backends())#len(hastats.get_backends_up())
+    data['haactive'] = len(hastats.get_backends_up())
     metrics.append(data)
+    time.sleep(sleeptime)
 
 def needed_servers(acu=None,diff=None):
     last = metrics[-1]
@@ -174,18 +195,24 @@ def new_metrics(current_cumulated, hareset=False):
         print "Calculation: float(%s) - float(%s) / float(%s-%s.seconds (%s))" % \
                 (str(current_cumulated), metrics[-1]['acu'], str(current['date']), str(metrics[-1]['date']),\
                 str((current['date'] - metrics[-1]['date']).seconds))
-        current['diff'] = (float(current_cumulated) - float(metrics[-1]['acu'])) \
-                / float((current['date']-metrics[-1]['date']).seconds)
+
+        if current_cumulated:
+            current['diff'] = int((float(current_cumulated) - float(metrics[-1]['acu'])) \
+                    / float((current['date']-metrics[-1]['date']).seconds))
+        else:
+            current['diff'] = 0
     except ZeroDivisionError:
         current['diff'] = 0
 
+    stack = openstack()
     current['needed'] = needed_servers(acu=current['acu'], diff=current['diff'])
-    current['active'] = len(hastats.get_backends_up())
+    current['active'] = len(stack.active_backends())#len(hastats.get_backends_up())
+    current['haactive'] = len(hastats.get_backends_up())
 
-    if hareset:
-        current['acu'] = 0
-        current['diff'] = metrics[-1]['diff']
-        current['needed'] = metrics[-1]['needed']
+    #if hareset:
+        #current['acu'] = current_cumulated
+        #current['diff'] = metrics[-1]['diff']
+        #current['needed'] = needed_servers(acu=current['acu'], diff=current['diff'])
 
     #stack = openstack()
     #current['active'] = stack.active_backends()
@@ -210,7 +237,6 @@ def main():
             initiate()
 
         while True:
-            time.sleep(sleeptime)
             current = new_metrics(hastats.get_backend_cum_requests()['stot'])
             print metrics
             print needed_servers()
@@ -234,8 +260,9 @@ def main():
                 # Sleeping
                 print "Sleeping one more round"
 
-            update_conf()
-            new_metrics(0, hareset=True)
+            if update_conf():
+                print "HAproxy config reloaded"
+            new_metrics(hastats.get_backend_cum_requests()['stot'], hareset=True)
 
             for line in hastats.get_stat_backends():
                 print line['svname'] + ', ' + line['status']
